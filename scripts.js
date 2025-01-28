db76e2d02c8239095b19e5853aa58477b716e2eb
Recblaster <irobuster123@gmail.com>
2025-01-28 12:23:06 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -47,10 +47,10 @@ window.onload = function () {
         } else if (frameIndex === 8) {
             createFloatingHearts();
             createTextBox(frame, 
-    "If you are reading this,that means things did work out between us,finally last baar baat karne vaala idea kaam kar hi gaya😆.So happy today ki I am able to give you this code.Last bday pe bas ek shayari likhi thi vo bhi block tha.Soo I wanted to give you something truly remarkable, something only I could have thought of, so it becomes an unforgettable memory........\n\n" +
-    "I LOVE YOU 💚\n\n" +
-    "                        ~Yours ausom Iro_xD😉",
-    "Hope You Liked this💚",
+    "If you are reading this,that means things did work out between us,finally last baar baat karne vaala idea kaam kar hi gaya😆.So happy today ki I am able to give you this code.Last bday pe bas ek shayari likhi thi vo bhi block tha.Soo I wanted to give you something truly remarkable, something only I could have thought of, so it becomes an unforgettable memory.<br><br>" +
+    "<strong>I LOVE YOU 💚</strong><br><br>" +
+    "<span style='margin-left: 20px;'>~Yours ausom Iro_xD😉</span>",
+    "Hope You Liked this🤍",
     function() {
         // No alert message here
     }
